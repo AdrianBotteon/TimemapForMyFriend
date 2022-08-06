ae8fa226f33084768eb2c9ca46205d648ee99baa
AdrianBotteon <adrianbotteon@gmail.com>
2022-08-07 03:34:10 +0800

    updated markers with date

--- src/components/space/carto/atoms/DefsMarkers.js
+++ src/components/space/carto/atoms/DefsMarkers.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+var date = new Date();
+var day = date.getDate();
+day = day < 10 ? "0" + day : day
+var month = date.getMonth() + 1;
+month = month < 10 ? "0" + month : month
+var year = date.getFullYear();
+var today = day + "/" + month + "/" + year;
+
+
 const MapDefsMarkers = ({ markers, projectPoint, narrative }) => {
   if (markers === undefined)
     return null;
@@ -8,16 +17,20 @@ const MapDefsMarkers = ({ markers, projectPoint, narrative }) => {
       {
         markers.map(marker => {
           const { x, y } = projectPoint([marker.latitude, marker.longitude])
-          return (
-            <svg>
-              <g
-                className={`location-event ${narrative ? "no-hover" : ""}`}
-                transform={`translate(${x}, ${y})`}
-              >
-                <circle cx="0" cy="0" r="10" stroke="black" stroke-width="3" fill="yellow" />
-              </g>
-            </svg>
-          )
+          if (marker.startdate <= today) {
+            return (
+              <svg>
+                <g
+                  className={`location-event ${narrative ? "no-hover" : ""}`}
+                  transform={`translate(${x}, ${y})`}
+                >
+                  <circle cx="0" cy="0" r="10" stroke="black" stroke-width="2" 
+                  fill= {marker.enddate>=today?"yellow":"none"} stroke-opacity="0.8" fill-opacity="0.8"/>
+                </g>
+              </svg>
+            )
+          }
+          else return null
         })
       }
     </>
